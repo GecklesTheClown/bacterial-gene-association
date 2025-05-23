47d7d8a7ee65d4646274fb657c77e85bc4097b62
GecklesTheClown <nicholas.gecks@proton.me>
2025-05-23 12:18:22 +1000

    main changes

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from gene-association!")
+from spmf import Spmf
+import pandas as pd
 
+def convert(df, use_names=False):
+    with open('transactions.txt', 'w') as f:
+        for _, row in df.iterrows():
+            if use_names:
+                items = [str(col) for col in df.columns if row[col] == 1]
+            else:
+                items = [str(i+1) for i, val in enumerate(row) if val == 1]
+            f.write(" ".join(items) + "\n")
+def main():
+    
+    df = pd.read_csv('data/DRX100168.csv').drop('FILE', axis=1)
+    convert(df)
+        
+    spmf = Spmf("NegFIN",
+                input_filename="transactions.txt",
+                output_filename="output.txt",
+                arguments=[0.8],
+                spmf_bin_location_dir="./")  # ← ✔ points to directory containing spmf.jar
+
+    spmf.run()   
+    
+    
 
 if __name__ == "__main__":
     main()
